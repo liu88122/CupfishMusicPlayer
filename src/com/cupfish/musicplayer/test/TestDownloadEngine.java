cb058620572e9f274ab7bfc3d1316b777a1fa170
liu88122 <liu88122@gmail.com>
2012-11-20 00:11:30 +0800

    下载引擎相关测试类

---
+++ src/com/cupfish/musicplayer/test/TestDownloadEngine.java
@@ -0,0 +1,34 @@
+package com.cupfish.musicplayer.test;
+
+import android.os.Environment;
+import android.test.AndroidTestCase;
+
+import com.cupfish.musicplayer.download.DownloadTask;
+import com.cupfish.musicplayer.download.DownloadTask.DownloadListener;
+
+public class TestDownloadEngine extends AndroidTestCase {
+
+	public void testDownload(){
+		String url = "http://www.cupfish.com/apk/CupfishMusicPlayer2.0.apk";
+		String dir = Environment.getExternalStorageDirectory() + "/cupfish";
+		DownloadTask task = new DownloadTask(getContext(), url, dir, 2);
+		task.addDownloadListener(new DownloadListener() {
+			
+			@Override
+			public void onDownloading(int size) {
+				// TODO Auto-generated method stub
+				System.out.println(size);
+				
+			}
+			
+			@Override
+			public void onDownloadFinish() {
+				// TODO Auto-generated method stub
+				System.out.println("Download finished");
+				
+			}
+		});
+		task.start();
+	}
+	
+}
--- src/com/cupfish/musicplayer/test/TestLRCReader.java
+++ src/com/cupfish/musicplayer/test/TestLRCReader.java
@@ -18,7 +18,7 @@ public class TestLRCReader extends AndroidTestCase {
 	public void testLrcRead() throws Exception{
 
 		Song song = BaiduTingHelper.getSongById("13869261");
-		TreeMap<Long, String> result = LRCReader.getLRC(song, getContext());
+		TreeMap<Long, String> result = LRCReader.getLRCTreeMap(song, getContext());
 		Iterator timeIterator = result.keySet().iterator();
 		while(timeIterator.hasNext()){
 			System.out.println(timeIterator.next());
@@ -26,7 +26,7 @@ public class TestLRCReader extends AndroidTestCase {
 	}
 	
 	public void testLrcReader2() throws Exception{
-		TreeMap<Long, String> result = LRCReader.parseLRC2(new File("/mnt/sdcard/cupfish/lrc/lanlianhua.lrc"));
+		TreeMap<Long, String> result = LRCReader.parseLRCFromFile(new File("/mnt/sdcard/cupfish/lrc/lanlianhua.lrc"));
 		System.out.println(result.size());
 		for(Map.Entry<Long, String> entry : result.entrySet()){
 			System.out.println(entry.getKey() +  ":" + entry.getValue());
