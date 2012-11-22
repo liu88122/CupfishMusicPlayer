8b205248b0d3301fd1115bf7a4ea643fa4fa6706
liu88122 <liu88122@gmail.com>
2012-11-22 17:47:29 +0800

    相关测试类变化

--- src/com/cupfish/musicplayer/test/TestLRCReader.java
+++ src/com/cupfish/musicplayer/test/TestLRCReader.java
@@ -10,15 +10,15 @@ import java.util.TreeMap;
 import android.test.AndroidTestCase;
 
 import com.cupfish.musicplayer.bean.Song;
+import com.cupfish.musicplayer.lrc.LrcReader;
 import com.cupfish.musicplayer.utils.BaiduTingHelper;
-import com.cupfish.musicplayer.utils.LRCReader;
 
 public class TestLRCReader extends AndroidTestCase {
 	
 	public void testLrcRead() throws Exception{
 
 		Song song = BaiduTingHelper.getSongById("13869261");
-		TreeMap<Long, String> result = LRCReader.getLRCTreeMap(song, getContext());
+		TreeMap<Long, String> result = LrcReader.getLRCTreeMap(song, getContext());
 		Iterator timeIterator = result.keySet().iterator();
 		while(timeIterator.hasNext()){
 			System.out.println(timeIterator.next());
@@ -26,7 +26,7 @@ public class TestLRCReader extends AndroidTestCase {
 	}
 	
 	public void testLrcReader2() throws Exception{
-		TreeMap<Long, String> result = LRCReader.parseLRCFromFile(new File("/mnt/sdcard/cupfish/lrc/lanlianhua.lrc"));
+		TreeMap<Long, String> result = LrcReader.parseLRCFromFile(new File("/mnt/sdcard/cupfish/lrc/lanlianhua.lrc"));
 		System.out.println(result.size());
 		for(Map.Entry<Long, String> entry : result.entrySet()){
 			System.out.println(entry.getKey() +  ":" + entry.getValue());
