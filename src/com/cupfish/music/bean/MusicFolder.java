946c38f05c65373c4a7cc9011e3e882da0517607
liu88122 <liu88122@gmail.com>
2013-01-11 17:20:54 +0800

    实现Parcelable接口

--- src/com/cupfish/music/bean/MusicFolder.java
+++ src/com/cupfish/music/bean/MusicFolder.java
@@ -1,6 +1,9 @@
 package com.cupfish.music.bean;
 
-public class MusicFolder {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MusicFolder implements Parcelable{
 
 	private String title;
 	private String path;
@@ -32,5 +35,35 @@ public class MusicFolder {
 	public void setCount(int count) {
 		this.count = count;
 	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(title);
+		dest.writeString(path);
+		dest.writeInt(count);
+	}
+
+	public static final Parcelable.Creator<MusicFolder> CREATOR = new Creator<MusicFolder>() {
+
+		@Override
+		public MusicFolder[] newArray(int size) {
+			
+			return new MusicFolder[size];
+		}
+
+		@Override
+		public MusicFolder createFromParcel(Parcel source) {
+			String title = source.readString();
+			String path = source.readString();
+			int count = source.readInt();
+			MusicFolder folder = new MusicFolder(title, path, count);
+			return folder;
+		}
+	};
 	
 }
