1d76eca0ea268e61a3b8e84086186e2f17308ce9
liu88122 <liu88122@gmail.com>
2013-01-11 17:22:25 +0800

    update

--- src/com/cupfish/music/bean/LocalItem.java
+++ src/com/cupfish/music/bean/LocalItem.java
@@ -1,5 +1,7 @@
 package com.cupfish.music.bean;
 
+import java.util.ArrayList;
+
 
 public class LocalItem {
 
@@ -8,6 +10,7 @@ public class LocalItem {
 	private int backgroundCover;
 	private int itemNum;
 	private Class<?> target;
+	private ArrayList data;
 	public int getItemNum() {
 		return itemNum;
 	}
@@ -17,8 +20,6 @@ public class LocalItem {
 		this.foregroundCover = foregroundCover;
 		this.backgroundCover = backgroundCover;
 	}
-
-
 
 	public void setItemNum(int itemNum) {
 		this.itemNum = itemNum;
@@ -46,6 +47,14 @@ public class LocalItem {
 
 	public void setTarget(Class<?> target) {
 		this.target = target;
+	}
+
+	public ArrayList getData() {
+		return data;
+	}
+
+	public void setData(ArrayList data) {
+		this.data = data;
 	}
 	
 }
