a4e9e4d3902a2a8d7bc080713c199392e443581f
liu88122 <liu88122@gmail.com>
2013-04-26 15:12:41 +0800

    update new slpash bg

--- src/com/cupfish/music/service/DownloadService.java
+++ src/com/cupfish/music/service/DownloadService.java
@@ -13,6 +13,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
